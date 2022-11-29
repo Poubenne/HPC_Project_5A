1ed74d07fc6873397d98a5d9b6f546edabab8f2d
Poubenne <poubenoui@gmail.com>
2022-11-29 09:16:14 +0100

    Final 4 version : measuring the sorting time + no TestCUDA

--- 4.cu
+++ 4.cu
@@ -53,8 +53,9 @@ __global__ void SortSmall_k(int **M, int j, int k) {
     * @param M The array of small arrays
     * @param N The number of small arrays
     * @param d The size of the small arrays (they all have the same)
+    * @return A float corresponding to the time taken by the sort
 */
-void SortSmall(int **M, size_t N, size_t d) {
+float SortSmall(int **M, size_t N, size_t d) {
 //Sorts a group of small arrays using GPU parallelized Bitonic Sort
 
     int **M_GPU;
@@ -64,18 +65,28 @@ void SortSmall(int **M, size_t N, size_t d) {
     for (int i = 0; i < N; i++){
         cudaMalloc(&tempo_array[i], d * sizeof(int));
         cudaMemcpy(tempo_array[i], M[i], d * sizeof(int), cudaMemcpyHostToDevice);
-    }
-    
+    }    
     cudaMemcpy(M_GPU, tempo_array, N*sizeof(int*), cudaMemcpyHostToDevice);
+
+//  Timing variables
+    float elapsed_time;
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
 
     int j, k;
     /* Major step */
+    cudaEventRecord(start, 0);
     for (k = 2; k <= d; k <<= 1) {
         /* Minor step */
         for (j=k>>1; j>0; j=j>>1) {
             SortSmall_k<<<N, d>>>(M_GPU, j, k);
         }
     }
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsed_time, start, stop);
+
 
     for (size_t i=0;i<N;i++) {
         cudaMemcpy(M[i], tempo_array[i], d * sizeof(int), cudaMemcpyDeviceToHost);
@@ -85,45 +96,43 @@ void SortSmall(int **M, size_t N, size_t d) {
         cudaFree(tempo_array[i]);
     }
     cudaFree(M_GPU);
+
+    return elapsed_time;
 }
 
 int main(void) {
     size_t N = 1024;
-    size_t d = 64;
+    size_t d;
+    size_t n_tries = 1000;
     int** array;
     
     array = (int**) malloc(N*sizeof(int*));
     
-    float TimerV;
-    cudaEvent_t start, stop;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
-
+    float total_time;
     
-    
-    printf("Measuring the performance on different d values :\n\tWarning! the performance measure takes into account the memory allocation time\n");
+    printf("Measuring the performance on different d values :\n");
     for (d=4; d<=1024; d*=2) {
-        for (size_t i=0;i<N;i++) {
-            GenerateUnsortedRandomArray(array+i, d);
-        }
+        
+        total_time = 0.0;
+        for (size_t i=0;i<n_tries;i++) {
+            for (size_t i=0;i<N;i++) {
+                GenerateUnsortedRandomArray(array+i, d);
+            }
 
-        cudaEventRecord(start, 0);
-        SortSmall(array, N, d);
-        cudaEventRecord(stop, 0);
-        cudaEventSynchronize(stop);
-        cudaEventElapsedTime(&TimerV, start, stop);
-        printf("d : %zu, time : %2f s\n", d,TimerV / 1000.0);
+            total_time += SortSmall(array, N, d);
 
-        for (size_t i=0;i<N;i++) {
-            if (! IsSortedAscending(array[i],d) ) {
-                printf("A sub-array isn't sorrted... i = %zd\n", i);
-                exit(EXIT_FAILURE);
+            for (size_t i=0;i<N;i++) {
+                if (! IsSortedAscending(array[i],d) ) {
+                    printf("A sub-array isn't sorted... i = %zd\n", i);
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            for (size_t i=0;i<N;i++) {
+                free(array[i]);
             }
         }
-
-        for (size_t i=0;i<N;i++) {
-            free(array[i]);
-        }
+        printf("d : %zu, time for %zu runs : %2f s\n", d,  n_tries, total_time / 1000.0);
     }
     printf("Done without errors!\n");
 
